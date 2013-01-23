1fb37824c5d3b4f00dc7b56d745b209464912a07
Peter Müller <munter@fumle.dk>
2013-01-23 14:57:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,123 @@
+var Path = require('path'),
+    fs = require('fs'),
+    relationLabelByType = {
+        HtmlScript: '<script>',
+        HtmlStyle: '<style>',
+        HtmlCacheManifest: '<html manifest>',
+        HtmlIFrame: '<iframe>',
+        HtmlFrame: '<frame>',
+        HtmlAlternateLink: '<link rel=alternate>',
+        HtmlConditionalComment: function (htmlConditionalComment) {return '<!--[if ' + htmlConditionalComment.condition + ']>';},
+        HtmlImage: '<img>',
+        HtmlAudio: '<audio>',
+        HtmlShortcutIcon: 'icon',
+        HtmlVideo: '<video>',
+        HtmlVideoPoster: '<video poster>',
+        HtmlEmbed: '<embed>',
+        HtmlApplet: '<applet>',
+        HtmlObject: '<object>',
+        HtmlEdgeSideInclude: '<esi:include>',
+        HtmlAnchor: '<a>',
+        HtmlRequireJsMain: 'data-main',
+        JavaScriptInclude: 'INCLUDE',
+        JavaScriptGetText: 'GETTEXT',
+        JavaScriptGetStaticUrl: 'GETSTATICURL',
+        JavaScriptAmdDefine: 'define',
+        JavaScriptAmdRequire: 'require',
+        CssImage: 'background-image',
+        CssImport: '@import',
+        CssBehavior: 'behavior',
+        CssFontFaceSrc: '@font-face src',
+        CssAlphaImageLoader: 'AlphaImageLoader'
+    };
+
+module.exports = function (name) {
+    name = (name || 'assetviz');
+    var targetFileName = name + '.html',
+        data = {
+            assets: [],
+            relations: []
+        };
+
+    return function drawGraph(assetGraph, cb) {
+        var idx = 0,
+            vizGraph = new assetGraph.constructor({ root: './tpl' }),
+            query = assetGraph.constructor.query;
+
+        assetGraph.findAssets().forEach(function (asset) {
+            if (asset.url || asset.outgoingRelations.length) {
+                asset.idx = idx;
+                data.assets.push({
+                    path: asset.url ? Path.relative(assetGraph.root, asset.url) : '',
+                    fileName: (asset.url ? Path.basename(asset.url) : 'i:' + asset).replace(/"/g, '\\"'),
+                    type: asset.type.toLowerCase()
+                });
+                idx += 1;
+            }
+        });
+
+        assetGraph.findRelations({}, true).forEach(function (relation) {
+            if ('idx' in relation.from && 'idx' in relation.to) {
+                var typeString = relationLabelByType[relation.type] || '';
+                if (typeof typeString === 'function') {
+                    typeString = typeString(relation);
+                }
+                data.relations.push({
+                    source: relation.from.idx,
+                    target: relation.to.idx,
+                    type: typeString
+                });
+            }
+        });
+
+        var dataString = 'var assetgraph = ' + JSON.stringify(data, undefined, 4);
+
+        vizGraph.on('error', function (err) {
+                console.error((err.asset ? err.asset.urlOrDescription + ': ' : '') + err.stack);
+                process.exit(1);
+            })
+            .loadAssets([
+                'index.html'
+            ])
+            .populate({
+                followRelations: {
+                    type: query.not(['HtmlAnchor'])
+                }
+            })
+            .queue(function injectAssetData(assetGraph) {
+                assetGraph.findAssets({
+                    type: 'JavaScript',
+                    fileName: 'data.js'
+                })[0].text = dataString;
+            })
+            .inlineRelations({
+                type: ['CssImage', 'HtmlImage', 'HtmlShortcutIcon', 'HtmlAppleTouchStartupImage']
+            })
+            .inlineRelations({
+                type: ['CssFontFaceSrc']
+            })
+            .inlineRelations({
+                type: ['CssBehavior']
+            })
+            /*
+            .compressJavaScript({type: 'JavaScript'}, 'uglifyJs')
+            .minifyAssets({
+                type: ['Html', 'JavaScript', 'Css']
+            })
+            */
+            // inline style and script
+            .inlineRelations({ type: ['HtmlStyle', 'HtmlScript'] })
+            .moveAssets({type: 'Html', isInline: false}, function (asset) {
+                return "file://" + Path.normalize(targetFileName);
+            })
+            .writeAssetsToDisc({url: /^file:/})
+            .writeStatsToStderr()
+            .run(function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+
+        cb();
+    };
+};
---
+++ tpl/graph.js
@@ -0,0 +1,57 @@
+/*global d3, assetgraph*/
+
+var svg = d3.select('.graph'),
+    force = d3.layout.force()
+        .nodes(d3.values(assetgraph.assets))
+        .links(assetgraph.relations)
+        .size([window.innerWidth, window.innerHeight]) // Some browsers have trouble reading dimensions of svg elements
+        .linkDistance(180)
+        .charge(-100);
+
+
+var edges = svg.append('g')
+    .attr('class', 'relations')
+    .selectAll('path')
+    .data(force.links())
+    .enter()
+        .append('path')
+        .attr('id', function (d, i) { return 'p' + i; });
+
+var edgeLabels = svg.append('g')
+    .attr('class', 'relationLabels')
+    .selectAll('text')
+    .data(force.links()).enter()
+        .append('text')
+        .attr('text-anchor', 'middle')
+            .append('textPath')
+            .attr('startOffset', '50%')
+            .attr('xlink:href', function (d, i) { return '#p' + i; })
+            .text(function (d) { return d.type; });
+
+var nodes = svg.append('g')
+    .attr('class', 'assets')
+    .selectAll('g')
+    .data(force.nodes()).enter()
+        .append('g');
+
+nodes.append('circle')
+    .attr('r', 6)
+    .attr('class', function (d) { return d.type; })
+    .call(force.drag);
+
+nodes.append('text')
+    .attr('text-anchor', 'middle')
+    .attr('dy', function () { return -10; })
+    .text(function (d) { return d.fileName; });
+
+
+force.on('tick', function () {
+    edges.attr('d', function (d) {
+        return "M" + d.source.x + "," + d.source.y +
+               " " + d.target.x + "," + d.target.y;
+    });
+
+    nodes.attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    });
+}).start();
