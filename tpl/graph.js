4602733f75056265f98fcfacefd7067a1194479b
Peter Müller <munter@fumle.dk>
2013-01-23 17:54:52 +0100

    Delay rendering until document has loaded

--- tpl/graph.js
+++ tpl/graph.js
@@ -1,57 +1,62 @@
 /*global d3, assetgraph*/
 
-var svg = d3.select('.graph'),
-    force = d3.layout.force()
-        .nodes(d3.values(assetgraph.assets))
-        .links(assetgraph.relations)
-        .size([window.innerWidth, window.innerHeight]) // Some browsers have trouble reading dimensions of svg elements
-        .linkDistance(180)
-        .charge(-100);
+
+debugger
+window.onload = function () {
+    var svg = d3.select('.graph'),
+        force = d3.layout.force()
+            .nodes(d3.values(assetgraph.assets))
+            .links(assetgraph.relations)
+            .size([window.innerWidth, window.innerHeight]) // Some browsers have trouble reading dimensions of svg elements
+            .gravity(.05)
+            .charge(-300)
+            .linkDistance(200);
 
 
-var edges = svg.append('g')
-    .attr('class', 'relations')
-    .selectAll('path')
-    .data(force.links())
-    .enter()
-        .append('path')
-        .attr('id', function (d, i) { return 'p' + i; });
+    var edges = svg.append('g')
+        .attr('class', 'relations')
+        .selectAll('path')
+        .data(force.links())
+        .enter()
+            .append('path')
+            .attr('id', function (d, i) { return 'p' + i; });
 
-var edgeLabels = svg.append('g')
-    .attr('class', 'relationLabels')
-    .selectAll('text')
-    .data(force.links()).enter()
-        .append('text')
+    var edgeLabels = svg.append('g')
+        .attr('class', 'relationLabels')
+        .selectAll('text')
+        .data(force.links()).enter()
+            .append('text')
+            .attr('text-anchor', 'middle')
+                .append('textPath')
+                .attr('startOffset', '50%')
+                .attr('xlink:href', function (d, i) { return '#p' + i; })
+                .text(function (d) { return d.type; });
+
+    var nodes = svg.append('g')
+        .attr('class', 'assets')
+        .selectAll('g')
+        .data(force.nodes()).enter()
+            .append('g');
+
+    nodes.append('circle')
+        .attr('r', 6)
+        .attr('class', function (d) { return d.type; })
+        .call(force.drag);
+
+    nodes.append('text')
         .attr('text-anchor', 'middle')
-            .append('textPath')
-            .attr('startOffset', '50%')
-            .attr('xlink:href', function (d, i) { return '#p' + i; })
-            .text(function (d) { return d.type; });
-
-var nodes = svg.append('g')
-    .attr('class', 'assets')
-    .selectAll('g')
-    .data(force.nodes()).enter()
-        .append('g');
-
-nodes.append('circle')
-    .attr('r', 6)
-    .attr('class', function (d) { return d.type; })
-    .call(force.drag);
-
-nodes.append('text')
-    .attr('text-anchor', 'middle')
-    .attr('dy', function () { return -10; })
-    .text(function (d) { return d.fileName; });
+        .attr('dy', function () { return -10; })
+        .text(function (d) { return d.fileName; });
 
 
-force.on('tick', function () {
-    edges.attr('d', function (d) {
-        return "M" + d.source.x + "," + d.source.y +
-               " " + d.target.x + "," + d.target.y;
-    });
+    force.on('tick', function () {
+        edges.attr('d', function (d) {
+            return "M" + d.source.x + "," + d.source.y +
+                   " " + d.target.x + "," + d.target.y;
+        });
 
-    nodes.attr("transform", function (d) {
-        return "translate(" + d.x + "," + d.y + ")";
-    });
-}).start();
+        nodes.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    }).start();
+};
